79893faa4d7e2dd76c138ad9e06358b71955666b
Allan Winckler <allan@gavea.com>
2023-11-26 23:33:36 -0300

    fix: multistep

--- packages/react/src/components/MultiStep/styles.ts
+++ packages/react/src/components/MultiStep/styles.ts
@@ -1,14 +1,45 @@
 import { styled } from '../../styles'
-import { Text } from '../Text'
 export const MultiStepContainer = styled('div', {
   width: '100%',
 })
 
-export const Label = styled(Text, {
-  color: '$brandSecondary50',
+export const Label = styled('p', {
+  fontFamily: '$default',
+  lineHeight: '$base',
+  margin: 0,
+  color: '$background60',
+
+  variants: {
+    size: {
+      xxs: { fontSize: '$xxa' },
+      xs: { fontSize: '$xs' },
+      sm: { fontSize: '$sm' },
+      md: { fontSize: '$md' },
+      lg: { fontSize: '$lg' },
+      xl: { fontSize: '$xl' },
+      '2xl': { fontSize: '$2xl' },
+      '4xl': { fontSize: '$4xl' },
+      '5xl': { fontSize: '$5xl' },
+      '6xl': { fontSize: '$6xl' },
+      '7xl': { fontSize: '$7xl' },
+      '8xl': { fontSize: '$8xl' },
+      '9xl': { fontSize: '$9xl' },
+    },
+    weight: {
+      regular: { fontWeight: '$regular' },
+      medium: { fontWeight: '$medium' },
+      bold: { fontWeight: '$bold' },
+    },
+    error: {
+      true: {
+        color: '$alertDanger50',
+      },
+    },
+  },
 
   defaultVariants: {
-    size: 'sm',
+    size: 'md',
+    weight: 'regular',
   },
 })
 
